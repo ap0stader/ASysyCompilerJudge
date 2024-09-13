f6bcc4aea0ad2a21f7fc73f8a5c1a746daae4aa1
ap0stader <ap0stader@outlook.com>
2024-09-13 11:33:51 +0800

    Update java & config

--- lang/java.py
+++ lang/java.py
@@ -11,20 +11,21 @@ from util.statuscode import StatusCode
 from util.termcolor import RESET, INVERSE
 
 __TEMP_BASE = Path("./runtime/java/")
-__TEMP_JAR = __TEMP_BASE / "Compiler.jar"
+__TEMP_JAR_PATH = __TEMP_BASE / "Compiler.jar"
 __TEMP_WORKDIR = __TEMP_BASE / "workdir"
-__TEMP_SOURCECODE = __TEMP_WORKDIR / "testfile.txt"
+__TEMP_SOURCECODE_PATH = __TEMP_WORKDIR / "testfile.txt"
 
 
 class ModifiedHandler(FileSystemEventHandler):
-    def __init__(self, executor: Executor, jar_name: str):
+    def __init__(self, executor: Executor, jar_name: str, temp_jar_path: Path):
         self.executor = executor
         self.jar_name = jar_name
+        self.temp_jar_path = temp_jar_path
 
     def on_created(self, event):
         if not event.is_directory and os.path.basename(event.src_path) == self.jar_name:
             print("New " + INVERSE + self.jar_name + RESET + " has been detected", end="")
-            shutil.copy(event.src_path, __TEMP_JAR)
+            shutil.copy(event.src_path, self.temp_jar_path)
             print(" and copied.")
             self.executor.start()
 
@@ -39,16 +40,16 @@ def get_observer(executor: Executor, jar_path: str):
     # 创建JAR文件的观察者
     observer = Observer()
     # 绑定事件处理器
-    handler = ModifiedHandler(executor, jar_name)
+    handler = ModifiedHandler(executor, jar_name, __TEMP_JAR_PATH)
     # 返回观察者
     return observer.schedule(handler, jar_dir)
 
 
-def execute(args: str, sourcecode_path: str, output_files_path: str) -> (StatusCode, str, str):
+def execute(args: str, sourcecode_path: str, compiler_all_output_path: str) -> (StatusCode, str, str):
     # 创建工作目录
     __TEMP_WORKDIR.mkdir(exist_ok=True)
     # 拷贝源代码文件
-    shutil.copy(sourcecode_path, __TEMP_SOURCECODE)
+    shutil.copy(sourcecode_path, __TEMP_SOURCECODE_PATH)
     # 创建进程
     command = ['java', '-jar', '../Compiler.jar', args]
     process = Popen(command, cwd=__TEMP_WORKDIR, stdout=PIPE, stderr=PIPE)
@@ -58,7 +59,7 @@ def execute(args: str, sourcecode_path: str, output_files_path: str) -> (StatusC
         stdout = stdout.decode().strip()
         stderr = stderr.decode().strip()
         # 转移工作目录
-        shutil.copy(__TEMP_WORKDIR, output_files_path)
+        shutil.copy(__TEMP_WORKDIR, compiler_all_output_path)
         shutil.rmtree(__TEMP_WORKDIR)
         # 判断是否有RE
         if stderr != "":
@@ -68,6 +69,6 @@ def execute(args: str, sourcecode_path: str, output_files_path: str) -> (StatusC
     except TimeoutExpired:
         process.kill()
         # 转移工作目录
-        shutil.copy(__TEMP_WORKDIR, output_files_path)
+        shutil.copy(__TEMP_WORKDIR, compiler_all_output_path)
         shutil.rmtree(__TEMP_WORKDIR)
         return StatusCode.EXECUTE_TLE, "", ""
